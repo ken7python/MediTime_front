a34c7bc1e1c161f268fa2817f5c5dd331968a499
ken7python <ken.1215.banana@gmaile.com>
2024-11-17 09:58:07 +0900

    11/17: Added image upload function

--- js/image.js
+++ js/image.js
@@ -39,12 +39,57 @@ function cancelUpload(){
 }
 
 function upload(){
-    alert(app.time + "の薬を登録しました。");
-    app.viewPhotoRadio = true;
-    app.registerPhotoRadio = true;
-    app.uploadDiv = false;
-    app.registerPhotoDiv = false;
-    app.imagePreview = null;
+    data = {label: app.time, image: app.imagePreview};
+    let param = {
+        method:  "POST",
+        headers: { "content-type": "application/json; charset=utf-8" },
+        body:    JSON.stringify( data ),
+    };
+    console.log(data);
+    let obj;
+    //alert(url);
+    fetch(url + "/uploadImage", param).then(async function(res){
+        obj = res.statusText;
+        console.log(obj);
+
+        alert(app.time + "の薬を登録しました。");
+        app.viewPhotoRadio = true;
+        app.registerPhotoRadio = true;
+        app.uploadDiv = false;
+        app.registerPhotoDiv = false;
+        app.imagePreview = null;
+        getImage();
+    }).catch(function(e){alert(e);});
+}
+
+function showImage(label){
+    app.showingLabel = label;
+    image = app.images.find(image => image.label == app.showingLabel);
+    if (image == null){
+        app.showingImage = null;
+        app.showingText = "登録なし";
+    }else{
+        app.showingImage = image.image;
+        app.showingText = null;
+    }
+}
+function deleteImage(){
+    data = {label: app.showingLabel};
+    let param = {
+        method:  "POST",
+        headers: { "content-type": "application/json; charset=utf-8" },
+        body:    JSON.stringify( data ),
+    };
+    console.log(data);
+    let obj;
+    fetch(url + "/deleteImage", param).then(async function(res){
+        obj = res.statusText;
+        console.log(obj);
+        
+        alert(app.showingLabel + "の薬を削除しました。");
+        getshowImage();
+        
+    }).catch(function(e){console.log(e);});
 }
 
 const app = new Vue({
@@ -57,5 +102,30 @@ const app = new Vue({
         registerPhotoDiv: false,
         uploadDiv: false,
         imagePreview: null,
+
+        images: [],
+        showingLabel: null,
+        showingImage: null,
+        showingText: null,
     },
 });
+function getImage(){
+    fetch(url + "/getImage").then(async function(res){
+        obj = await res.json();
+        obj = obj.image;
+        console.log(obj);
+        app.images = obj;
+    }).catch(function(e){alert(e);});
+}
+getImage();
+
+
+function getshowImage(){
+    fetch(url + "/getImage").then(async function(res){
+        obj = await res.json();
+        obj = obj.image;
+        console.log(obj);
+        app.images = obj;
+        showImage(app.showingLabel);
+    }).catch(function(e){alert(e);});
+}
